5fd2774415bbf856baa9fe3e363ecc3ad59c672c
Unknown <smart.Julia@yandex.ru>
2018-05-06 06:41:42 +0500

    Added some markdown for reactions

--- components/chat/Messages/Message/MessageBox/MessageBox.js
+++ components/chat/Messages/Message/MessageBox/MessageBox.js
@@ -3,8 +3,13 @@ import moment from 'moment';
 const ReactMarkdown = require('react-markdown');
 import Lightbox from 'react-image-lightbox';
 import ImageMessage from './imageMessage/imageMessage';
+import { Picker } from 'emoji-mart';
+import Reactions from '../MessageBox/Reactions/Reactions';
 
+import 'emoji-mart/css/emoji-mart.css';
 import './styles.css';
+
+const maxAmount = 6;
 
 export default class Message extends React.Component {
     constructor(props) {
@@ -23,9 +28,17 @@ export default class Message extends React.Component {
             saveElementForScroll: props.saveElementForScroll,
             curTime: props.date && !isNaN(props.date) && (
                 moment(props.date).fromNow()
-            )
+            ),
+            reactions: [{ emoji: 'barber', amount: 1, self: 'pressed' },
+                { emoji: 'crystal_ball', amount: 3, self: 'not-pressed' }],
+            showPicker: false,
+            showPickerButton: false
         };
+        this.onEmojiSelect = this.onEmojiSelect.bind(this);
+        this.onShowPickerButtonClick = this.onShowPickerButtonClick.bind(this);
+        this.checkForZeros = this.checkForZeros.bind(this);
     }
+
     componentDidMount() {
         setInterval(() => {
             this.setState({
@@ -36,24 +49,72 @@ export default class Message extends React.Component {
         }, 60000);
     }
 
+    onShowPickerButtonClick() {
+        this.setState({
+            showPicker: !this.state.showPicker
+        });
+    }
+
+    isUpdated(emoji) {
+        for (let reaction of this.state.reactions) {
+            if (reaction.emoji === emoji && reaction.self === 'not-pressed') {
+                reaction.amount++;
+                reaction.self = 'pressed';
+
+                return true;
+            } else if (reaction.emoji === emoji) {
+                reaction.amount--;
+                reaction.self = 'not-pressed';
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    checkForZeros() {
+        for (let i = 0; i < this.state.reactions.length; i++) {
+            if (this.state.reactions[i].amount === 0) {
+                this.state.reactions.splice(i, 1);
+
+                return;
+            }
+        }
+    }
+
+    onEmojiSelect(emoji) {
+        // здесь запрос в axois
+        if (!this.isUpdated(emoji.id)) {
+            this.state.reactions.push({ emoji: emoji.id, amount: 1, self: 'pressed' });
+        }
+        this.checkForZeros();
+        this.state.reactions.splice(maxAmount);
+        this.setState({
+            reactions: this.state.reactions,
+            showPicker: !this.state.showPicker
+        });
+    }
 
     render() {
-        return <div>
+        return <div className="messages-container">
             <li
-                className = {this.state.position} >
+                className={this.state.position}>
                 <div
-                    className = 'avatar' > <img
+                    className='avatar' > <img
                         onClick={() => this.setState({ isAvatarOpen: true })}
-                        src = {this.state.avatar}
-                        draggable = 'false'/>
+                        src={this.state.avatar}
+                        draggable='false' />
                     {this.state.isAvatarOpen && (
                         <Lightbox
                             mainSrc={this.state.avatar}
                             onCloseRequest={() => this.setState({ isAvatarOpen: false })}
-                            imageCaption = {this.state.author}
+                            imageCaption={this.state.author}
                         />
                     )}</div>
-                <div className='msg'>
+                <div className='msg'
+                    onMouseEnter={() => this.setState({ showPickerButton: true })}
+                    onMouseLeave={() => this.setState({ showPickerButton: false })}>
                     <a
                         className='message__user-link'
                         onClick={this.props.onTitleClick}>{this.state.author}</a>
@@ -64,6 +125,12 @@ export default class Message extends React.Component {
                         />)
                         : null
                     }
+                    {this.state.showPickerButton
+                        ? <div className="show-picker-button"
+                            onClick={this.onShowPickerButtonClick}
+                        >&#9786;
+                        </div>
+                        : null}
                     <ReactMarkdown renderers={{
                         linkReference: (reference) => {
                             if (!reference.href) {
@@ -75,7 +142,30 @@ export default class Message extends React.Component {
                     }}
                     source={this.state.text} />
                     <time>{this.state.curTime}</time>
+                    <br />
+                    <Reactions className="reactions" reactions={this.state.reactions} />
                 </div>
+                {this.state.showPicker
+                    ? <Picker
+                        onClick={this.onEmojiSelect}
+                        showPreview={false}
+                        color='lightsalmon'
+                        set='emojione'
+                        perLine='6'
+                        style={{
+                            zIndex: '100',
+                            right: '7%',
+                            position: 'absolute',
+                            margin: '8%',
+                            top: '18px',
+                            height: '150px',
+                            overflow: 'hidden'
+                        }}
+                        l18n={{ search: null }}
+                        exclude={['recent']}
+                    />
+                    : null
+                }
             </li>
             {this.props.renderAddCmp()}
 
---
+++ components/chat/Messages/Message/MessageBox/Reactions/Reactions.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import 'emoji-mart/css/emoji-mart.css';
+import './styles.css';
+import { Emoji } from 'emoji-mart';
+
+export default class Reactions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { reactions: props.reactions };
+        this.onReactionClick = this.onReactionClick.bind(this);
+        this.update = this.update.bind(this);
+        this.checkForZeros = this.checkForZeros.bind(this);
+    }
+
+    update(emoji) {
+        for (let reaction of this.state.reactions) {
+            if (reaction.emoji === emoji && reaction.self === 'not-pressed') {
+                reaction.amount++;
+                reaction.self = 'pressed';
+            } else if (reaction.emoji === emoji) {
+                reaction.amount--;
+                reaction.self = 'not-pressed';
+            }
+        }
+    }
+
+    checkForZeros() {
+        for (let i = 0; i < this.state.reactions.length; i++) {
+            if (this.state.reactions[i].amount === 0) {
+                this.state.reactions.splice(i, 1);
+
+                return;
+            }
+        }
+    }
+
+    onReactionClick(emoji) {
+        // тут запрос в axios
+        this.update(emoji);
+        this.checkForZeros();
+        this.setState({
+            reactions: this.state.reactions,
+            showPicker: !this.state.showPicker
+        });
+    }
+
+    render() {
+        return (<div className="reactions">
+            {this.state.reactions.map((reaction, idx) =>
+                (
+                    <div key={idx} className={`container ${reaction.self}`}
+                        onClick={() => this.onReactionClick(reaction.emoji)}>
+                        <Emoji className="emoji" key={idx} emoji={reaction.emoji} size={16} />
+                        <div className="amount">{reaction.amount}</div>
+                    </div>
+                )
+            )}
+        </div>);
+    }
+}
