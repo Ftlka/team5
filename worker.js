a2b93e567f5019a761f208b06c20d28fde684ba7
Данил Ишин <ishindanil96@gmail.com>
2018-05-19 20:06:35 +0500

    handle notification click

--- worker.js
+++ worker.js
@@ -17,3 +17,25 @@ self.addEventListener('push', e => {
         })
     );
 });
+
+self.addEventListener('notificationclick', e => {
+    e.notification.close();
+
+    let path = e.target.origin + '/';
+    e.waitUntil(
+        clients.matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        }).then(clientList => {
+            for (const client of clientList) {
+                if (client.url === path && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
